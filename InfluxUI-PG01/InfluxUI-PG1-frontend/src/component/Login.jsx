48a2afdd4fd3233002b9e0a3b4c2887a6c50609a
iscoYuan <coookieeee1998@gmail.com>
2024-08-27 14:28:32 +0930

    login page

--- InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/Login.jsx
+++ InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/Login.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
-import {Button,TextField,Typography,Box} from "@mui/material";
+import {
+    Container,
+    Box,
+    TextField,
+    Button,
+    Typography,
+    Card,
+    CardContent,
+    Avatar,
+    Alert,
+    FormControlLabel,
+    Checkbox,
+    createTheme,
+    ThemeProvider
+} from '@mui/material';
 import axios from "axios";
-function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+import PersonIcon from '@mui/icons-material/Person';
+import { enUS } from '@mui/material/locale';
+import userData from '../users.json';
+const theme = createTheme({}, enUS);
 
-    const handleSubmit = async (event) => {
+
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+
+    const handleSubmit_back = async (event) => {
         event.preventDefault();
         console.log('Username:', username);
         console.log('Password:', password);
@@ -29,52 +53,102 @@ function Login() {
 
         }
     };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const user = userData.users.find(u => u.username === username.trim() && u.password === password);
+
+        if (user) {
+            console.log("Login successful");
+            setSuccessMessage("Login successful!");
+            setErrorMessage("");
+        } else {
+            setErrorMessage("Incorrect username or password");
+            setSuccessMessage("");
+        }
+    };
 
     return (
-        <Box
-            sx={{
-                width: '100%',
-                maxWidth: '400px',
-                margin: 'auto',
-                padding: 4,
-                backgroundColor: '#ffffff',
-                borderRadius: 2,
-                boxShadow: 3,
-                marginTop: '30px'
-            }}
-            className="login-container">
-            <Typography component="h1" variant="h5">
-                Sign in
-            </Typography>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <TextField
-                        className="textFiled"
-                        label="username"
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <TextField
-                        label="password"
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <Button variant="contained"
-                        sx={{
-                            marginTop: '30px'
-                        }}
-                        type="submit">Login</Button>
-            </form>
-        </Box>
+        <ThemeProvider theme={theme}>
+            <Container maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <PersonIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Login
+                    </Typography>
+                    <Card sx={{ mt: 3, boxShadow: 5 }}>
+                        <CardContent>
+                            <form onSubmit={handleSubmit}>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="username"
+                                    label="Username"
+                                    name="username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    autoComplete="username"
+                                    autoFocus
+                                />
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    id="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    autoComplete="current-password"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={showPassword}
+                                            onChange={() => setShowPassword(!showPassword)}
+                                            color="primary"
+                                        />
+                                    }
+                                    label="Show password"
+                                />
+                                {errorMessage && (
+                                    <Alert severity="error" sx={{ mt: 2 }}>
+                                        {errorMessage}
+                                    </Alert>
+                                )}
+                                {successMessage && (
+                                    <Alert severity="success" sx={{ mt: 2 }}>
+                                        {successMessage}
+                                    </Alert>
+                                )}
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Login
+                                </Button>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
 
