a2eb1bd0d1919e080950792795c2f659e7d16be0
iscoYuan <coookieeee1998@gmail.com>
2024-09-09 16:31:43 +0930

    grafana create new dashboard

--- InfluxUI-PG01/InfluxUI-PG1-backend/src/main/resources/sql/querylog.sql
+++ InfluxUI-PG01/InfluxUI-PG1-backend/src/main/resources/sql/querylog.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS public.query_log
 (
-    id character varying(255) COLLATE pg_catalog."default" NOT NULL DEFAULT nextval('query_log_id_seq'::regclass),
+    id character varying(255) COLLATE pg_catalog."default" NOT NULL,
     user_id character varying(255) COLLATE pg_catalog."default",
     "timestamp" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     bucket character varying(255) COLLATE pg_catalog."default",
@@ -13,12 +13,7 @@ CREATE TABLE IF NOT EXISTS public.query_log
     tags character varying(255) COLLATE pg_catalog."default",
     query_duration character varying(255) COLLATE pg_catalog."default",
     result_status character varying(255) COLLATE pg_catalog."default",
-    result_count character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT query_log_pkey PRIMARY KEY (id),
-    CONSTRAINT query_log_user_id_fkey FOREIGN KEY (user_id)
-        REFERENCES public.app_user (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    result_count character varying(255) COLLATE pg_catalog."default"
 )
 
 TABLESPACE pg_default;
