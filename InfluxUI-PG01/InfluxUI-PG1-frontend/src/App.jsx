25ec1148e87f84d92be2651de25203ccefb7d6d5
Michael <tomjjh@icloud.com>
2024-09-15 12:33:33 +0930

    axios runQuery

--- InfluxUI-PG01/InfluxUI-PG1-frontend/src/App.jsx
+++ InfluxUI-PG01/InfluxUI-PG1-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-
+import axios from 'axios';
 import Login from './component/Login.jsx'
 import Dashboard from './component/Dashboard.jsx';
 import {Typography} from "@mui/material";
--- InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/Dashboard.jsx
+++ InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/Dashboard.jsx
@@ -16,6 +16,85 @@ import {
 
 
 function Dashboard() {
+<<<<<<< Updated upstream
+=======
+    const { buckets, measurements, fields, setBuckets, setMeasurements, setFields, initialBuckets, initialMeasurements, initialFields } = useFetchedData();
+
+    const [selectedBuckets, setSelectedBuckets] = useState([]);
+    const [selectedMeasurements, setSelectedMeasurements] = useState([]);
+    const [selectedFields, setSelectedFields] = useState([]);
+    const [visualizationData, setVisualizationData] = useState(null);
+
+    //generate code based on fetched elements
+    const [generatedCode, setGeneratedCode] = useState("");
+
+    // Drag and Drop feature
+    const handleDrop = (type, item) => {
+        if (type === 'bucket') {
+            // Ensure the bucket is not already in the selected list
+            if (!selectedBuckets.includes(item.label)) {
+                setSelectedBuckets(prevBuckets => [...prevBuckets, item.label]);
+                setBuckets(prevBuckets => prevBuckets.filter(bucket => bucket !== item.label));
+            }
+        } else if (type === 'measurement') {
+            if (!selectedMeasurements.includes(item.label)) {
+                setSelectedMeasurements(prevMeasurements => [...prevMeasurements, item.label]);
+                setMeasurements(prevMeasurements => prevMeasurements.filter(measurement => measurement !== item.label));
+            }
+        } else if (type === 'field') {
+            if (!selectedFields.includes(item.label)) {
+                setSelectedFields(prevFields => [...prevFields, item.label]);
+                setFields(prevFields => prevFields.filter(field => field !== item.label));
+            }
+        }
+    };
+
+    // Clear All function
+    const clearAll = () => {
+        // Reset selected items and generated code
+        setSelectedBuckets([]);
+        setSelectedMeasurements([]);
+        setSelectedFields([]);
+        setGeneratedCode("");
+
+        //Reset data
+        setBuckets(initialBuckets);
+        setMeasurements(initialMeasurements);
+        setFields(initialFields);
+    }
+
+    // Function to update the generated code
+    useEffect(() => {
+        const updateGeneratedCode = () => {
+            let code = "";
+            if (selectedBuckets && selectedBuckets.length > 0) {
+                selectedBuckets.forEach(bucket => {
+                    code += `from(bucket: "${bucket}")\n`;
+                });
+            }
+            // Code for time range
+            // code += "    |> range(start: -1h)\n";
+
+            if (selectedMeasurements && selectedMeasurements.length > 0) {
+                selectedMeasurements.forEach(measurement => {
+                    code += `    |> filter(fn: (r) => r._measurement == "${measurement}")\n`;
+                });
+            }
+
+            if (selectedFields && selectedFields.length > 0) {
+                selectedFields.forEach(field => {
+                    code += `    |> filter(fn: (r) => r._field == "${field}")\n`;
+                });
+            }
+
+            setGeneratedCode(code);
+        };
+
+        updateGeneratedCode();
+
+    }, [selectedBuckets, selectedMeasurements, selectedFields]);
+
+>>>>>>> Stashed changes
     const handleSubmit = async (event) => {
         event.preventDefault();
         // console.log('Username:', username);
@@ -40,6 +119,30 @@ function Dashboard() {
 
         }
     };
+<<<<<<< Updated upstream
+=======
+
+    const runQuery = async () => {
+        try {
+            await handleSubmit();
+
+            const dataToSend = {
+                buckets: selectedBuckets,
+                measurements: selectedMeasurements,
+                fields: selectedFields,
+                query: generatedCode
+            };
+
+            const response = await axios.post("http://localhost:1808/api/runQuery", dataToSend);
+            if (response.status === 200) {
+                setVisualizationData(response.data);
+            }
+        } catch (error) {
+            console.error("Error running query", error);
+        }
+    };
+
+>>>>>>> Stashed changes
     return (
         <div className="dashsoard-container">
 
@@ -122,9 +225,15 @@ function Dashboard() {
         <div className="main-iner-box" style={{height: "50%", marginTop: "5px"}}>
             <textarea className="text-area" placeholder="Write your query here" ></textarea>
             <div className="btn-box">
+<<<<<<< Updated upstream
                 <Button variant="contained" color="error" style={{margin: "0 5px"}}>Clear All</Button>
                 <Button variant="contained" style={{margin: "0 5px"}}>Copy Cpde</Button>
                 <Button variant="contained" color="success" style={{margin: "0 5px"}}>Run Query</Button>
+=======
+                <Button variant="contained" color="error" style={{margin: "0 5px"}} onClick={clearAll}>Clear All</Button>
+                <Button variant="contained" style={{margin: "0 5px"}}>Copy Code</Button>
+                <Button variant="contained" color="success" style={{margin: "0 5px"}} onClick={runQuery}>Run Query</Button>
+>>>>>>> Stashed changes
             </div>
         </div>
         
