65c2a6085b8c8ba657b35d5334f387fe60c5c5af
Ziyan Zhao <ziyanzhao@ZiyandeMacBook-Air.local>
2024-09-29 18:46:59 +0930

    Add new home page

---
+++ InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/DashboardNew.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import DragDropPage from './DragDropPage'; 
+import { Box, Typography } from '@mui/material';
+import CodeArea from './CodeArea'; 
+
+const DashboardNew = () => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Box 
+        style={{ 
+          display: 'flex', 
+          flexDirection: 'row', 
+          height: 'calc(100vh - 60px)',
+          overflow: 'hidden' 
+        }}
+      >
+        <Box 
+          style={{ 
+            flexBasis: '40%',
+            padding: '10px', 
+            borderRight: '1px solid #ccc', 
+            overflowY: 'scroll', 
+            maxHeight: '100%' 
+          }}
+        >
+          <DragDropPage />
+        </Box>
+
+        <Box 
+          style={{ 
+            flexBasis: '60%',  
+            display: 'flex', 
+            flexDirection: 'column', 
+            padding: '10px', 
+          }}
+        >
+          <Box 
+            style={{ 
+              flexBasis: '60%',  
+              padding: '10px',
+              borderBottom: '1px solid #ccc', 
+            }}
+          >
+            <Typography variant="h6">Graph Area</Typography>
+          </Box>
+
+          <Box 
+            style={{ 
+              flexBasis: '40%',  
+              padding: '10px',
+            }}
+          >
+            <CodeArea/> 
+          </Box>
+        </Box>
+      </Box>
+    </DndProvider>
+  );
+};
+
+export default DashboardNew;
