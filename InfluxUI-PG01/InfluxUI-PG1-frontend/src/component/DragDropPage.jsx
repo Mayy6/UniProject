93afba9eead6a15b5a0e9fd73c851e2bace2bdb3
Manhong Chen <manhong.chen@student.adelaide.edu.au>
2024-10-11 23:07:43 +1030

    1.modified the tag selection 2.add filter function for tag values 3. modified the query generation

--- InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/DragDropPage.jsx
+++ InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/DragDropPage.jsx
@@ -33,6 +33,8 @@ const DragDropPage = () => {
       customEndTime: new Date(),
       open: false,
       bucketFiles: [],
+      tagOptions: {}, 
+      selectedTagValues: {},
     };
   }
 
@@ -54,7 +56,7 @@ const DragDropPage = () => {
       setActiveTabIndex((prevIndex) => prevIndex - 1);
     }
   };
-  
+
   const currentTab = tabs[activeTabIndex];
 
   const loadBucketFiles = (tabIndex) => {
@@ -96,31 +98,52 @@ const DragDropPage = () => {
       .then((data) => {
         const selectedData = data.find(item => item._measurement === measurement);
         if (selectedData) {
-          const newTags = selectedData.tags.flatMap((tag) =>
-            Object.entries(tag).map(([key, values]) => 
-              values.map((value) => `${measurement}.${key} = "${value}"`)
-            ).flat()
-          );
+          const newTags = selectedData.tags.map((tag) => {
+            return Object.keys(tag).map(key => `${measurement}.${key}`);
+          }).flat();
+
+          const tagOptions = selectedData.tags.reduce((acc, tag) => {
+            Object.entries(tag).forEach(([key, values]) => {
+              acc[`${measurement}.${key}`] = values;
+            });
+            return acc;
+          }, {});
+
           const newFields = selectedData.fields.map((field) => `${measurement}.${field}`);
-  
+
           const updatedTabs = [...tabs];
           updatedTabs[tabIndex].tags = Array.from(new Set([...updatedTabs[tabIndex].tags, ...newTags]));
           updatedTabs[tabIndex].fields = Array.from(new Set([...updatedTabs[tabIndex].fields, ...newFields]));
+          updatedTabs[tabIndex].tagOptions = tagOptions; 
           setTabs(updatedTabs);
         }
       })
       .catch((error) => console.error('Error generating tags and fields:', error));
   };
-  
+
+  const handleUpdateTagSelections = (tag, selectedValues, tabIndex) => {
+    const updatedTabs = [...tabs];
+    updatedTabs[tabIndex].selectedTagValues = {
+      ...updatedTabs[tabIndex].selectedTagValues,
+      [tag]: selectedValues,
+    };
+    setTabs(updatedTabs);
+  };
 
   const handleDrop = (droppedItem, tabIndex) => {
     const { item, type } = droppedItem;
     const updatedTabs = [...tabs];
+
     if (type === 'measurement' && !updatedTabs[tabIndex].rightMeasurements.includes(item)) {
       updatedTabs[tabIndex].rightMeasurements.push(item);
       generateTagsAndFields(item, tabIndex);
     } else if (type === 'tag' && !updatedTabs[tabIndex].rightTags.includes(item)) {
       updatedTabs[tabIndex].rightTags.push(item);
+      const tagOptions = updatedTabs[tabIndex].tagOptions[item];
+      updatedTabs[tabIndex].selectedTagValues = {
+        ...updatedTabs[tabIndex].selectedTagValues,
+        [item]: tagOptions, 
+      };
     } else if (type === 'field' && !updatedTabs[tabIndex].rightFields.includes(item)) {
       updatedTabs[tabIndex].rightFields.push(item);
     }
@@ -129,6 +152,7 @@ const DragDropPage = () => {
 
   const removeItem = (item, tabIndex) => {
     const updatedTabs = [...tabs];
+
     if (updatedTabs[tabIndex].rightMeasurements.includes(item)) {
       const relatedTags = updatedTabs[tabIndex].tags.filter(tag => tag.startsWith(`${item}.`));
       const relatedFields = updatedTabs[tabIndex].fields.filter(field => field.startsWith(`${item}.`));
@@ -137,8 +161,14 @@ const DragDropPage = () => {
       updatedTabs[tabIndex].fields = updatedTabs[tabIndex].fields.filter(field => !relatedFields.includes(field));
       updatedTabs[tabIndex].rightMeasurements = updatedTabs[tabIndex].rightMeasurements.filter(m => m !== item);
     }
-    updatedTabs[tabIndex].rightTags = updatedTabs[tabIndex].rightTags.filter(t => t !== item);
+
+    if (updatedTabs[tabIndex].rightTags.includes(item)) {
+      updatedTabs[tabIndex].rightTags = updatedTabs[tabIndex].rightTags.filter(t => t !== item);
+      delete updatedTabs[tabIndex].selectedTagValues[item];
+    }
+
     updatedTabs[tabIndex].rightFields = updatedTabs[tabIndex].rightFields.filter(f => f !== item);
+
     setTabs(updatedTabs);
   };
 
@@ -197,7 +227,6 @@ const DragDropPage = () => {
                       loadDataFromFile(e.target.value, activeTabIndex);
                     }}
                   >
-
                     {currentTab.bucketFiles.map((file, index) => (
                       <MenuItem key={index} value={file}>{file}</MenuItem>
                     ))}
@@ -278,6 +307,7 @@ const DragDropPage = () => {
                     <MenuItem value="Last 24 hours">Last 24 hours</MenuItem>
                     <MenuItem value="Last 7 days">Last 7 days</MenuItem>
                     <MenuItem value="Custom Time Range">Custom Time Range</MenuItem>
+                    <MenuItem value="None">None</MenuItem>
                   </Select>
                 </FormControl>
               </Box>
@@ -330,6 +360,9 @@ const DragDropPage = () => {
                   onDrop={(droppedItem) => handleDrop(droppedItem, activeTabIndex)}
                   items={currentTab.rightTags}
                   removeItem={(item) => removeItem(item, activeTabIndex)}
+                  tagOptions={currentTab.tagOptions || {}}
+                  updateTagSelections={(tag, selectedValues) => handleUpdateTagSelections(tag, selectedValues, activeTabIndex)}
+                  selectedTagValues={currentTab.selectedTagValues || {}}
                   boxHeight={'200px'}
                 />
               </Box>
@@ -348,7 +381,7 @@ const DragDropPage = () => {
               <QueryGenerator
                 fileName={currentTab.selectedFile}
                 measurements={currentTab.rightMeasurements}
-                tags={currentTab.rightTags}
+                tags={currentTab.selectedTagValues || {}} 
                 fields={currentTab.rightFields}
                 timeRange={currentTab.selectedTimeRange}
                 customStartTime={currentTab.customStartTime}
