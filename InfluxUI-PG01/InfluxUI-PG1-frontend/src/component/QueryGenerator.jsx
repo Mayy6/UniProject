67e6cb24afdaeef88d81663e8ae904ca127f671d
yuanyinkai <yinkai.yuan@student.adelaide.edu.au>
2024-10-14 16:00:11 +1030

    custom time range

--- InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/QueryGenerator.jsx
+++ InfluxUI-PG01/InfluxUI-PG1-frontend/src/component/QueryGenerator.jsx
@@ -18,7 +18,7 @@ const QueryGenerator = ({ fileName, measurements, tags, fields, timeRange, custo
         const day = String(date.getDate()).padStart(2, '0');
         const hours = String(date.getHours()).padStart(2, '0');
         const minutes = String(date.getMinutes()).padStart(2, '0');
-        return `${year}-${month}-${day}T${hours}:${minutes}:00`;
+        return `${year}-${month}-${day}T${hours}:${minutes}:00+10:30`;
       };
 
       const customStartTimeLocal = formatLocalTime(customStartTime);
